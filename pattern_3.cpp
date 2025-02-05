604d147bc58bf871f8345d7454a02d1683d7725d
rishabkm <119279619+rishabkm@users.noreply.github.com>
2025-02-05 22:58:48 +0530

    Add files via upload

---
+++ pattern_3.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"The Pattern"<<endl;
+    for(int i=0;i<10;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
